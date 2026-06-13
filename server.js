36c9cd1862d76a8b5f47f606746b1f41b5f38fcb
Dima Pan <dimdish4@gmail.com>
2026-06-13 13:03:28 +0300

    Init commit

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
-const sun = (a, b) => a + b;
+const express = require("express");
+const bodyParser = require("body-parser");
 
-console.log(sum(10, 5));
+const app = express();
+
+const port = 3000;
+
+//midleware
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(
+    `Server listening on port $(port) and starting at http://localhost:$(app.config.port)`,
+  );
+});
